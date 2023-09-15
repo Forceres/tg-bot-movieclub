23a3cb9e9b15e017c44ab8c1f8e975e5175db4d9
Forceres <ilya.sereda.2001@gmail.com>
2023-09-15 21:36:27 +0300

    feat: change main.py for the test

--- src/main.py
+++ src/main.py
@@ -1,9 +1,22 @@
-import asyncio
+from telegram.ext import Application, CommandHandler, ContextTypes
+from telegram import Update
+from os import getenv
+from dotenv import load_dotenv
+from logger.logger import setup_logger
+
+load_dotenv()
 
 
-async def main():
-    pass
+async def start(updater: Update, context: ContextTypes.DEFAULT_TYPE):
+    return await updater.message.reply_text("Hello, forceres!")
+
+
+def main():
+    setup_logger()
+    application = Application.builder().token(getenv("TOKEN")).build()
+    application.add_handler(CommandHandler("start", start))
+    application.run_polling()
 
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
