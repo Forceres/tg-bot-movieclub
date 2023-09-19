4326991132edeac33c8ad12bb6d5aa48997181bf
Forceres <ilya.sereda.2001@gmail.com>
2023-09-19 15:49:16 +0300

    feat: add hooks for starting up bot and shutting down (init db and clean resources)

---
+++ src/utils/hooks.py
@@ -0,0 +1,16 @@
+import logging
+
+from telegram.ext import Application
+
+from src.db.db import SqliteRepository
+
+
+async def startup(application: Application) -> None:
+    logging.info("Starting up Bot!")
+    await (await SqliteRepository()).close_cursor()
+    if not SqliteRepository.check_connection():
+        await application.shutdown()
+
+
+async def shutdown(_application: Application) -> None:
+    await SqliteRepository.close()
