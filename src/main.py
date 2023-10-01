58cf8d368e210829bca1fd87eea427c5cffacb72
Forceres <ilya.sereda.2001@gmail.com>
2023-10-01 14:43:48 +0300

    feat: add config file with env data, add postinit and postshutdown hooks, replaced all load_dotenv with Config

--- src/main.py
+++ src/main.py
@@ -1,19 +1,24 @@
 from telegram.ext import Application, CommandHandler, ContextTypes
 from telegram import Update
-from os import getenv
-from dotenv import load_dotenv
+
 from logger.logger import setup_logger
+from config import Config
+from utils.hooks import startup, shutdown
 
-load_dotenv()
 
-
-async def start(updater: Update, context: ContextTypes.DEFAULT_TYPE):
+async def start(updater: Update, _context: ContextTypes.DEFAULT_TYPE):
     return await updater.message.reply_text("Hello, forceres!")
 
 
 def main():
     setup_logger()
-    application = Application.builder().token(getenv("TOKEN")).build()
+    application = (
+        Application.builder()
+        .token(Config.TOKEN.value)
+        .post_init(startup)
+        .post_stop(shutdown)
+        .build()
+    )
     application.add_handler(CommandHandler("start", start))
     application.run_polling()
 
