01b214222f66c3d8b3cf627c95518586ab0584b6
Forceres <ilya.sereda.2001@gmail.com>
2023-10-08 12:42:44 +0300

    refactor: create the rest structure of the project, delete help command

--- src/commands/help.py
+++
@@ -1,6 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-
-async def get_help(updater: Update, _context: ContextTypes.DEFAULT_TYPE):
-    return await updater.message.reply_text("Hello, forceres!")
