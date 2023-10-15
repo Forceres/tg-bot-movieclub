67e02f36cbaf68c05fd9b4cbf98e1750cd2382c2
Forceres <ilya.sereda.2001@gmail.com>
2023-10-15 19:57:57 +0300

    fix: redesign database

--- src/db/migrations/db_create.sql
+++ src/db/migrations/db_create.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS allowed_users
 (
     id         INTEGER PRIMARY KEY,
     tg_id      BIGINT                              NOT NULL UNIQUE,
+    admin BOOLEAN,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
@@ -10,11 +11,13 @@ CREATE TABLE IF NOT EXISTS movies
     id           INTEGER PRIMARY KEY,
     title        VARCHAR                             NOT NULL UNIQUE,
     description  VARCHAR,
-    link         VARCHAR                             NOT NULL UNIQUE,
+    director     VARCHAR,
     year         INTEGER                             NOT NULL,
+    countries    VARCHAR,
+    link         VARCHAR                             NOT NULL UNIQUE,
     duration     INTEGER,
-    imdb_rating       FLOAT,
-    rating FLOAT,
+    imdb_rating  FLOAT,
+    rating       FLOAT,
     start_watch  TIMESTAMP,
     finish_watch TIMESTAMP,
     created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
@@ -23,31 +26,32 @@ CREATE TABLE IF NOT EXISTS movies
 CREATE TABLE IF NOT EXISTS votings
 (
     id          INTEGER PRIMARY KEY,
-    winner_id   INTEGER   NOT NULL,
-    type        VARCHAR   NOT NULL CHECK ( type == 'schulze' OR type == 'asc' OR type == 'desc'),
-    created_at  TIMESTAMP NOT NULL,
+    winner_id   INTEGER,
+    type        VARCHAR NOT NULL CHECK ( type == 'schulze' OR type == 'asc' OR type == 'desc'),
+    created_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
     finished_at TIMESTAMP,
     FOREIGN KEY (winner_id) REFERENCES movies (id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS sessions(
+CREATE TABLE IF NOT EXISTS movies_votings (
     id INTEGER PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL,
-    finished_at TIMESTAMP,
     movie_id INTEGER,
-    FOREIGN KEY (movie_id) REFERENCES movies (id) ON DELETE CASCADE
-
+    voting_id INTEGER,
+    FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE,
+    FOREIGN KEY (voting_id) REFERENCES votings(id) ON DELETE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS current_voting
+CREATE TABLE IF NOT EXISTS sessions
 (
-    id               INTEGER PRIMARY KEY,
-    voting_id        INTEGER,
-    first_entity_id  INTEGER,
-    second_entity_id INTEGER,
-    third_entity_id  INTEGER,
-    FOREIGN KEY (voting_id) REFERENCES votings (id) ON DELETE CASCADE,
-    FOREIGN KEY (first_entity_id) REFERENCES movies (id) ON DELETE CASCADE,
-    FOREIGN KEY (second_entity_id) REFERENCES movies (id) ON DELETE CASCADE,
-    FOREIGN KEY (third_entity_id) REFERENCES movies (id) ON DELETE CASCADE
+    id              INTEGER PRIMARY KEY,
+    created_at   TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+    finished_at     TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS movies_sessions (
+    id INTEGER PRIMARY KEY,
+    movie_id INTEGER,
+    session_id INTEGER,
+    FOREIGN KEY (movie_id) REFERENCES movies(id) ON DELETE CASCADE,
+    FOREIGN KEY (session_id) REFERENCES sessions(id) ON DELETE CASCADE
 );
