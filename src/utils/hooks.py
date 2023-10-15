e12998ad99aa37653a9ce19c377f7f176ac2ca7f
Forceres <ilya.sereda.2001@gmail.com>
2023-10-15 19:59:55 +0300

    feat: enable sqlite3 foreign keys via pragma

--- src/utils/hooks.py
+++ src/utils/hooks.py
@@ -3,11 +3,13 @@ import logging
 from telegram.ext import Application
 
 from src.db.config.db import SqliteRepository
+from src.utils.queries import Queries
 
 
 async def startup(application: Application) -> None:
     logging.info("Starting up Bot!")
-    await (await SqliteRepository()).close_cursor()
+    async with SqliteRepository() as db:
+        await db.single_query(Queries.ENABLE_FOREIGN_KEYS.value)
     if not SqliteRepository.check_connection():
         await application.shutdown()
 
