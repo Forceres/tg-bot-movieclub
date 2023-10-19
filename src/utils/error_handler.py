dd9962f5182c3e74d6572712702a129d987de928
Forceres <ilya.sereda.2001@gmail.com>
2023-10-20 00:50:52 +0300

    fix: fix the behavior of the error_handler (right processing RetryAfter exception, and other types of exceptions)

--- src/utils/error_handler.py
+++ src/utils/error_handler.py
@@ -1,12 +1,29 @@
+import asyncio
 from logging import getLogger
 
 from telegram import Update
-from telegram.ext import ContextTypes
+from telegram.error import BadRequest, RetryAfter, TelegramError
+from telegram.ext import CallbackContext
+
 
 logger = getLogger(__name__)
 
 
-async def error_handler(_update: Update, context: ContextTypes.DEFAULT_TYPE):
-    logger.error(
-        "Exception caused while handling an update: ", exc_info=context.error
-    )
+async def error_handler(_update: Update, context: CallbackContext):
+    if isinstance(context.error, RetryAfter):
+        logger.error(
+            "RetryAfter Exception", exc_info=(RetryAfter, context.error, None)
+        )
+        await asyncio.sleep(context.error.retry_after)
+    elif isinstance(context.error, BadRequest):
+        logger.error(
+            "Error while handling an update: ",
+            exc_info=(BadRequest, context.error, None),
+        )
+    elif isinstance(context.error, TelegramError):
+        logger.error(
+            "Error while handling an update: ",
+            exc_info=(TelegramError, context.error, None),
+        )
+    else:
+        raise context.error
