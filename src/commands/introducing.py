d98ac5d61b7d5c906762922e20bf0c37bca4b0e2
Forceres <forceres31@gmail.com>
2023-10-24 23:03:41 +0300

    feat: add first version of help command

---
+++ src/commands/introducing.py
@@ -0,0 +1,27 @@
+from telegram import Update
+from telegram.ext import ContextTypes
+from telegram.constants import ParseMode
+
+from src.utils.authentication import authentication
+
+
+@authentication
+async def get_help(update: Update, _context: ContextTypes.DEFAULT_TYPE):
+    message = r"""*Приветствую вас\! Это справка о командах бота\.*
+_Добавить фильм в предложку можно с помощью отправки сообщения такого вида:_
+_*\#предлагаю*_ https://www\.kinopoisk\.ru/film/здесь\-указан\-id\-фильма/
+_Если хотите предложить несколько фильмов,
+тогда располагайте ссылки через запятую или пробелы\!_
+_В случае, если была передана ссылка на фильм,
+который уже был просмотрен ранее, тогда он будет внесен в предложку,
+то есть, его дата предложения изменится на текущую\!_
+Список возможных команд:
+/now \- вывести список фильмов текущего сеанса
+/cd \- перенести дату обсуждения фильма
+/dd \- вернуть классическое расписание
+/already \- получить ссылки с списком просмотренных фильмов
+/vote \- создать голосование \(только администратор\)
+    """
+    return await update.message.chat.send_message(
+        message, parse_mode=ParseMode.MARKDOWN_V2
+    )
