30346303638583d82b69e8099eb0b85da255b9ca
Forceres <ilya.sereda.2001@gmail.com>
2023-10-26 22:23:57 +0300

    refactor: change global db logs to warning level, remove all logger expressions from db services

--- src/db/services/getting.py
+++ src/db/services/getting.py
@@ -1,4 +1,3 @@
-from logging import getLogger
 from sqlite3 import Row
 from typing import Iterable
 
@@ -7,18 +6,13 @@ from aiosqlite import Cursor
 from src.db.config.db import SqliteRepository
 from src.utils.queries import Queries
 
-logger = getLogger(__name__)
-
 
 async def retrieve_current_session_movies() -> Iterable[Row]:
     async with SqliteRepository() as db:
-        result: Cursor = await db.single_query(
+        cursor: Cursor = await db.single_query(
             Queries.GET_CURRENT_MOVIES.value
         )
-        output: Iterable[Row] = await result.fetchall()
-        logger.warning(
-            "Query completed! -> {}".format(Queries.GET_CURRENT_MOVIES.value)
-        )
+        output: Iterable[Row] = await cursor.fetchall()
     return output
 
 
@@ -30,21 +24,13 @@ async def retrieve_suggested_movies() -> Iterable[Row] | bool:
         if cursor is None:
             return False
         output: Iterable[Row] = await cursor.fetchall()
-        logger.warning(
-            "Query completed! -> {}".format(Queries.GET_SUGGESTED_MOVIES.value)
-        )
     return output
 
 
 async def retrieve_already_watched_movies() -> Iterable[Row]:
     async with SqliteRepository() as db:
-        result: Cursor = await db.single_query(
+        cursor: Cursor = await db.single_query(
             Queries.GET_ALREADY_WATCHED_MOVIES.value
         )
-        output: Iterable[Row] = await result.fetchall()
-        logger.warning(
-            "Query completed! -> {}".format(
-                Queries.GET_ALREADY_WATCHED_MOVIES.value
-            )
-        )
+        output: Iterable[Row] = await cursor.fetchall()
     return output
