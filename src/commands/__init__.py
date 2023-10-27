fb0e48fd343fabbd4ed8a0d3817f98d35bce90f6
Forceres <forceres31@gmail.com>
2023-10-27 18:58:20 +0300

    chore: apply conversation handler

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -1,7 +1,19 @@
-from telegram.ext import CommandHandler, MessageHandler
+from telegram.ext import (
+    CommandHandler,
+    MessageHandler,
+    ConversationHandler,
+    CallbackQueryHandler,
+)
 from telegram.ext.filters import Regex
 
-from src.commands.creating import create_voting, suggest_movie
+from src.commands.creating import (
+    suggest_movie,
+    create_voting_type_keyboard,
+    base_settings_button_callback,
+    paginate_movies_button_callback,
+    get_suggestions_and_create_voting,
+    cancel,
+)
 from src.commands.getting import (
     change_watch_date,
     get_already_watched_movies_links,
@@ -19,12 +31,26 @@ def get_all_handlers() -> list:
         CommandHandler(
             "custom", define_custom_movie_description, has_args=True
         ),
-        CommandHandler("vote", create_voting),
         CommandHandler("now", get_current_movies),
         MessageHandler(
             Regex(r"^#предлагаю\s")
             & Regex(r"https://www.kinopoisk.ru/film/\d+/[\s|,]?"),
             suggest_movie,
+        ),
+        ConversationHandler(
+            entry_points=[CommandHandler("vote", create_voting_type_keyboard)],
+            states={
+                1: [CallbackQueryHandler(base_settings_button_callback)],
+                2: [
+                    CallbackQueryHandler(paginate_movies_button_callback),
+                    MessageHandler(
+                        Regex(r"(\d{1,3}(?:,\s*\d{1,3})(?![\d\s|[\w|\W]))")
+                        | Regex(r"(^\d{1,3}$)"),
+                        get_suggestions_and_create_voting,
+                    ),
+                ],
+            },
+            fallbacks=[CommandHandler("cancel", cancel)],
         ),
     ]
     return handlers
