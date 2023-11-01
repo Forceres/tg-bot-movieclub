60773aba71229b391dbeb66f24cf9a9e09f01569
Forceres <ilya.sereda.2001@gmail.com>
2023-11-01 08:46:08 +0300

    feat: add date_helper get_relative_date func, add this func to jobqueue on application level

--- src/main.py
+++ src/main.py
@@ -2,9 +2,12 @@ from telegram import Update
 from telegram.ext import Application
 from telegraph.aio import Telegraph
 
+from datetime import time
+
 from src.commands import get_all_handlers
 from src.logger.logger import setup_logger
 from src.config import Config
+from src.utils.date_helper import get_relative_date
 from src.utils.hooks import startup, shutdown
 from src.utils.error_handler import error_handler
 
@@ -19,6 +22,7 @@ def main():
         .build()
     )
     application.bot_data["telegraph"] = Telegraph()
+    application.job_queue.run_daily(get_relative_date, time=time(hour=18))
     application.add_handlers(get_all_handlers())
     application.add_error_handler(error_handler)
     application.run_polling(allowed_updates=Update.ALL_TYPES, read_timeout=30)
