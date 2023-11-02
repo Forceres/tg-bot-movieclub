e4b53848d2624454faf52b96dd2cd26ffcad9c61
Forceres <ilya.sereda.2001@gmail.com>
2023-11-03 00:35:46 +0300

    feat: add date_helper to get relative date using current time, add some queries for voting and after voting, refactor some prev queries

---
+++ src/utils/date_helper.py
@@ -0,0 +1,26 @@
+from datetime import datetime, timedelta
+
+from telegram.ext import ContextTypes
+
+
+def get_relative_date(context: ContextTypes.DEFAULT_TYPE):
+    current_datetime = datetime.now()
+    weekday = datetime.weekday(datetime.now())
+    date = ""
+    if weekday == 3:
+        date = datetime.isoformat(
+            (timedelta(7) + current_datetime).replace(hour=22, minute=0)
+        )
+    elif weekday < 3:
+        date = datetime.isoformat(
+            (timedelta(3 - weekday) + current_datetime).replace(
+                hour=22, minute=0
+            )
+        )
+    elif weekday > 3:
+        date = datetime.isoformat(
+            (timedelta(7 - (weekday - 3)) + current_datetime).replace(
+                hour=22, minute=0
+            )
+        )
+    context.bot_data["date"] = date
