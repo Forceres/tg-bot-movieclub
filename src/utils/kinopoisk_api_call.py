51ae4f947260b8b4adb6af037abae302d32f825c
Forceres <ilya.sereda.2001@gmail.com>
2023-11-05 22:13:03 +0300

    refactor: change some logic in suggest_new funcs, update api kinopoisk version and some little refactor changes

--- src/utils/kinopoisk_api_call.py
+++ src/utils/kinopoisk_api_call.py
@@ -10,7 +10,7 @@ logger = getLogger(__name__)
 
 async def api_call(ids: list) -> list[Response]:
     headers = {"X-API-KEY": Config.KINOPOISK_API_KEY.value}
-    url = "https://api.kinopoisk.dev/v1.3/movie/%s"
+    url = "https://api.kinopoisk.dev/v1.4/movie/%s"
     async with AsyncClient() as client:
         tasks = [
             create_task(client.get(url=url % curr_id, headers=headers))
